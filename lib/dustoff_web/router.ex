2dc3ecb6fec138c7dc334d691086690790b93b0b
Mike Zornek <zorn@zornlabs.com>
2025-05-26 15:53:42 -0400

    Remove unused path.

--- lib/dustoff_web/router.ex
+++ lib/dustoff_web/router.ex
@@ -73,7 +73,6 @@ defmodule DustoffWeb.Router do
     pipe_through [:browser]
 
     get "/users/log-in", UserSessionController, :new
-    get "/users/log-in/:token", UserSessionController, :confirm
     post "/users/log-in", UserSessionController, :create
     delete "/users/log-out", UserSessionController, :delete
   end
