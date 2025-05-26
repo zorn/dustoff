e628bad72efae022cc47a45d048149ebde1fc808
Mike Zornek <zorn@zornlabs.com>
2025-05-26 10:57:27 -0400

    Removes magic link related tests.

--- test/dustoff_web/controllers/user_registration_controller_test.exs
+++ test/dustoff_web/controllers/user_registration_controller_test.exs
@@ -20,22 +20,6 @@ defmodule DustoffWeb.UserRegistrationControllerTest do
   end
 
   describe "POST /users/register" do
-    @tag :capture_log
-    test "creates account but does not log in", %{conn: conn} do
-      email = unique_user_email()
-
-      conn =
-        post(conn, ~p"/users/register", %{
-          "user" => valid_user_attributes(email: email)
-        })
-
-      refute get_session(conn, :user_token)
-      assert redirected_to(conn) == ~p"/users/log-in"
-
-      assert conn.assigns.flash["info"] =~
-               ~r/An email was sent to .*, please access it to confirm your account/
-    end
-
     test "render errors for invalid data", %{conn: conn} do
       conn =
         post(conn, ~p"/users/register", %{
--- test/dustoff_web/controllers/user_session_controller_test.exs
+++ test/dustoff_web/controllers/user_session_controller_test.exs
@@ -2,7 +2,6 @@ defmodule DustoffWeb.UserSessionControllerTest do
   use DustoffWeb.ConnCase, async: true
 
   import Dustoff.AccountsFixtures
-  alias Dustoff.Accounts
 
   setup do
     %{unconfirmed_user: unconfirmed_user_fixture(), user: user_fixture()}
@@ -38,38 +37,6 @@ defmodule DustoffWeb.UserSessionControllerTest do
       assert response =~ "Log in"
       assert response =~ ~p"/users/register"
       assert response =~ "Log in with email"
-    end
-  end
-
-  describe "GET /users/log-in/:token" do
-    # test "renders confirmation page for unconfirmed user", %{conn: conn, unconfirmed_user: user} do
-    #   token =
-    #     extract_user_token(fn url ->
-    #       Accounts.deliver_login_instructions(user, url)
-    #     end)
-
-    #   conn = get(conn, ~p"/users/log-in/#{token}")
-    #   assert html_response(conn, 200) =~ "Confirm my account"
-    # end
-
-    test "renders login page for confirmed user", %{conn: conn, user: user} do
-      token =
-        extract_user_token(fn url ->
-          Accounts.deliver_login_instructions(user, url)
-        end)
-
-      conn = get(conn, ~p"/users/log-in/#{token}")
-      html = html_response(conn, 200)
-      refute html =~ "Confirm my account"
-      assert html =~ "Log in"
-    end
-
-    test "raises error for invalid token", %{conn: conn} do
-      conn = get(conn, ~p"/users/log-in/invalid-token")
-      assert redirected_to(conn) == ~p"/users/log-in"
-
-      assert Phoenix.Flash.get(conn.assigns.flash, :error) ==
-               "Magic link is invalid or it has expired."
     end
   end
 
@@ -135,70 +102,6 @@ defmodule DustoffWeb.UserSessionControllerTest do
       response = html_response(conn, 200)
       assert response =~ "Log in"
       assert response =~ "Invalid email or password"
-    end
-  end
-
-  describe "POST /users/log-in - magic link" do
-    test "sends magic link email when user exists", %{conn: conn, user: user} do
-      conn =
-        post(conn, ~p"/users/log-in", %{
-          "user" => %{"email" => user.email}
-        })
-
-      assert Phoenix.Flash.get(conn.assigns.flash, :info) =~ "If your email is in our system"
-      assert Dustoff.Repo.get_by!(Accounts.UserToken, user_id: user.id).context == "login"
-    end
-
-    test "logs the user in", %{conn: conn, user: user} do
-      {token, _hashed_token} = generate_user_magic_link_token(user)
-
-      conn =
-        post(conn, ~p"/users/log-in", %{
-          "user" => %{"token" => token}
-        })
-
-      assert get_session(conn, :user_token)
-      assert redirected_to(conn) == ~p"/"
-
-      # Now do a logged in request and assert on the menu
-      conn = get(conn, ~p"/")
-      response = html_response(conn, 200)
-      assert response =~ user.email
-      assert response =~ ~p"/users/settings"
-      assert response =~ ~p"/users/log-out"
-    end
-
-    test "confirms unconfirmed user", %{conn: conn, unconfirmed_user: user} do
-      {token, _hashed_token} = generate_user_magic_link_token(user)
-      refute user.confirmed_at
-
-      conn =
-        post(conn, ~p"/users/log-in", %{
-          "user" => %{"token" => token},
-          "_action" => "confirmed"
-        })
-
-      assert get_session(conn, :user_token)
-      assert redirected_to(conn) == ~p"/"
-      assert Phoenix.Flash.get(conn.assigns.flash, :info) =~ "User confirmed successfully."
-
-      assert Accounts.get_user!(user.id).confirmed_at
-
-      # Now do a logged in request and assert on the menu
-      conn = get(conn, ~p"/")
-      response = html_response(conn, 200)
-      assert response =~ user.email
-      assert response =~ ~p"/users/settings"
-      assert response =~ ~p"/users/log-out"
-    end
-
-    test "emits error message when magic link is invalid", %{conn: conn} do
-      conn =
-        post(conn, ~p"/users/log-in", %{
-          "user" => %{"token" => "invalid"}
-        })
-
-      assert html_response(conn, 200) =~ "The link is invalid or it has expired."
     end
   end
 
