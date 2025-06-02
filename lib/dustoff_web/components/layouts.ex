3330757d9fb17cce8b662c8ffb451358ec32792f
Mike Zornek <zorn@zornlabs.com>
2025-06-02 16:44:15 -0400

    Allow for wider display of content.

--- lib/dustoff_web/components/layouts.ex
+++ lib/dustoff_web/components/layouts.ex
@@ -71,7 +71,7 @@ defmodule DustoffWeb.Layouts do
     </header>
 
     <main class="px-4 py-20 sm:px-6 lg:px-8">
-      <div class="mx-auto max-w-2xl space-y-4">
+      <div class="mx-auto max-w-6xl space-y-4">
         {render_slot(@inner_block)}
       </div>
     </main>
