0486ee610eeb178a6b2453de6f53b333b29e75e8
Mike Zornek <zorn@zornlabs.com>
2025-06-03 11:21:14 -0400

    Update nullability of columns.

--- priv/repo/migrations/20250527182516_create_articles.exs
+++ priv/repo/migrations/20250527182516_create_articles.exs
@@ -4,10 +4,13 @@ defmodule Dustoff.Repo.Migrations.CreateArticles do
   def change do
     create table(:articles, primary_key: false) do
       add :id, :binary_id, primary_key: true
-      add :title, :string
-      add :body, :text
-      add :published_at, :utc_datetime_usec
-      add :author_id, references(:users, type: :binary_id, on_delete: :delete_all)
+      add :title, :string, null: false
+      add :body, :text, null: false
+      add :published_at, :utc_datetime_usec, null: true
+
+      add :author_id,
+          references(:users, type: :binary_id, on_delete: :delete_all),
+          null: false
 
       timestamps(type: :utc_datetime_usec)
     end
